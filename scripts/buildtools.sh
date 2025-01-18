fdeaed54c0d310fd05a4691b3237ef6346a79aa1
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2025-01-19 07:23:12 +0900

    buildtools.sh, mkliveiamge.sh: fix typo.

--- scripts/buildtools.sh
+++ scripts/buildtools.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 #
 # Copyright (c) 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018,
-#  2019 2020, 2023, 2024, 2025 Izumi Tsutsui.
+#  2019, 2020, 2023, 2024, 2025 Izumi Tsutsui.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
